2552bd438dbd07c604d88490374641e4ea4a85dc
Pujitha <chpujitha14@gmail.com>
2023-06-27 10:00:57 +0000

    commit message

---
+++ names/index.js
@@ -0,0 +1,6 @@
+const list = require("../country/state/city/index.js");
+const getFirstNames = require("../utilities/utils/index.js");
+const getPeopleInCity = (list) => {
+  return getFirstNames(list);
+};
+module.exports = getPeopleInCity;
